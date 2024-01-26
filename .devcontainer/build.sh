54b26d0a6cda825a7c47cd58a6b9d7d299f452e2
Kotochleb <krzy.wojciecho@gmail.com>
2024-01-26 10:15:09 +0000

    Remove packages to skipp

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -5,7 +5,6 @@ cd /home/geppetto/ros2_ws
 
 colcon build \
         --symlink-install \
-        --packages-skip ign_ros2_control_demos ignition-gazebo6 \
         --parallel-workers $(nproc) \
         --cmake-args \
             "-DCMAKE_BUILD_TYPE=Release" \
